c4e84c9cde73751122931ad1ff39dd34aa4dbea3
rjrodger <richard@ricebridge.com>
2019-02-21 22:11:20 +0000

    v0.1.0

--- entity-rtag.js
+++ entity-rtag.js
@@ -1,9 +1,7 @@
 /* Copyright (c) 2019 voxgig and other contributors, MIT License */
 'use strict'
 
-
 const Util = require('util')
-
 
 module.exports = entity_rtag
 module.exports.defaults = {
@@ -13,25 +11,22 @@ module.exports.defaults = {
   // TODO: mem-store should deep clone!
   clone_before_hydrate: true
 }
-module.exports.errors = {
-}
+module.exports.errors = {}
 
 function entity_rtag(options) {
   const seneca = this
   const Joi = seneca.util.Joi
-  const rtag = seneca.util.Nid({length:options.rtaglen})
-  
+  const rtag = seneca.util.Nid({ length: options.rtaglen })
+
   const HIT = 1
   const MISS = 2
-  
+
   seneca
     .message('role:entity,cmd:save', cmd_save_rtag)
     .message('role:cache,resolve:rtag', resolve_rtag)
 
-
-  
   Object.assign(cmd_save_rtag, {
-    desc: 'Override role:entity,cmd:save to rtag field.',
+    desc: 'Override role:entity,cmd:save to rtag field.'
   })
 
   async function cmd_save_rtag(msg) {
@@ -39,7 +34,6 @@ function entity_rtag(options) {
     ent.rtag = rtag() // always override
     return await this.prior(msg)
   }
-
 
   Object.assign(resolve_rtag, {
     desc: 'Use rtag to load cached version of expensive result.',
@@ -53,43 +47,41 @@ function entity_rtag(options) {
 
   async function resolve_rtag(msg) {
     const seneca = this
-    
+
     const space = msg.space
     const key = msg.key
     const rtag = msg.rtag
     const resolver = msg.resolver
 
-    const id = space+'~'+key+'~'+rtag
+    const id = space + '~' + key + '~' + rtag
     var cache_entry = await seneca.entity('sys/cache').load$(id)
 
-    if(cache_entry) {
+    if (cache_entry) {
       var entrydata = cache_entry.data
       var entryout = entrydata
-      
+
       // TODO: need a general entity hydration util as also needed by transport
-      if(entrydata.__entity$) {
-        if(options.clone_before_hydrate) {
+      if (entrydata.__entity$) {
+        if (options.clone_before_hydrate) {
           entrydata = Object.assign({}, entrydata)
         }
-        
+
         entrydata.entity$ = entrydata.__entity$
         delete entrydata.__entity$
         entryout = seneca.entity(entrydata)
       }
 
-      if(options.annotate) {
+      if (options.annotate) {
         entryout.rtag$ = HIT
       }
 
       return entryout
-    }
-    else {
+    } else {
       var origdata = await resolver.call(seneca)
       var cachedata = origdata
-      
-      if(cachedata && false !== cachedata.rtag_cache$) {
 
-        if(cachedata.entity$) {
+      if (cachedata && false !== cachedata.rtag_cache$) {
+        if (cachedata.entity$) {
           cachedata = cachedata.data$()
 
           // Avoid seneca-entity auto replacement of entities with id
@@ -98,20 +90,19 @@ function entity_rtag(options) {
         }
 
         cache_entry = seneca.entity('sys/cache').make$({
-          id$:id,
-          when:Date.now()
+          id$: id,
+          when: Date.now()
         })
 
         cache_entry.data = cachedata
         await cache_entry.save$()
       }
 
-      if(options.annotate) {
+      if (options.annotate) {
         origdata.rtag$ = MISS
       }
 
       return origdata
     }
   }
-
 }
--- test/entity-rtag.test.js
+++ test/entity-rtag.test.js
@@ -25,12 +25,14 @@ lab.test('plugin-load', async () => {
 
 lab.test('happy', async () => {
   var si = seneca_instance()
-  var foo_a = await si.entity('foo',{a:1}).save$()
-  expect(foo_a).contains({a:1})
-  expect(foo_a.rtag.length).equal(si.find_plugin('entity_rtag').defaults.rtaglen)
+  var foo_a = await si.entity('foo', { a: 1 }).save$()
+  expect(foo_a).contains({ a: 1 })
+  expect(foo_a.rtag.length).equal(
+    si.find_plugin('entity_rtag').defaults.rtaglen
+  )
 
   var first = foo_a.rtag
-  
+
   var foo_b = await foo_a.save$()
   var second = foo_b.rtag
 
@@ -40,30 +42,35 @@ lab.test('happy', async () => {
   expect(foo_a.rtag).equal(foo_b.rtag)
 })
 
-
 lab.test('resolve', async () => {
   var si = seneca_instance()
 
   var HIT = 1
   var MISS = 2
-  
+
   function make_resolver(foo_id) {
     return async function resolver() {
       var foo = await si.entity('foo').load$(foo_id)
       var bar = await si.entity('bar').load$(foo.bar_id)
-      var out = {foo:foo,bar:bar}
+      var out = { foo: foo, bar: bar }
 
-      if(foo.test_no_cache) {
+      if (foo.test_no_cache) {
         out.rtag_cache$ = false
       }
-      
+
       return out
     }
   }
 
-  var bar0 = await si.entity('bar').make$({b:1}).save$()
-  var foo0 = await si.entity('foo').make$({handle:'foo0',a:1, bar_id:bar0.id}).save$()
-  
+  var bar0 = await si
+    .entity('bar')
+    .make$({ b: 1 })
+    .save$()
+  var foo0 = await si
+    .entity('foo')
+    .make$({ handle: 'foo0', a: 1, bar_id: bar0.id })
+    .save$()
+
   var out0 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
     rtag: foo0.rtag,
@@ -73,7 +80,7 @@ lab.test('resolve', async () => {
   expect(out0.foo.a).equal(1)
   expect(out0.bar.b).equal(1)
   expect(out0.rtag$).equal(MISS)
-  
+
   var out1 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
     rtag: foo0.rtag,
@@ -83,7 +90,7 @@ lab.test('resolve', async () => {
   expect(out1.foo.a).equal(1)
   expect(out1.bar.b).equal(1)
   expect(out1.rtag$).equal(HIT)
-  
+
   bar0.b = 2
   await bar0.save$()
   await foo0.save$() // kicks rtag
@@ -97,7 +104,6 @@ lab.test('resolve', async () => {
   expect(out2.foo.a).equal(1)
   expect(out2.bar.b).equal(2)
   expect(out2.rtag$).equal(MISS)
-  
 
   var out3 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
@@ -109,7 +115,6 @@ lab.test('resolve', async () => {
   expect(out3.bar.b).equal(2)
   expect(out3.rtag$).equal(HIT)
 
-
   var out4 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
     rtag: foo0.rtag,
@@ -119,7 +124,6 @@ lab.test('resolve', async () => {
   expect(out4.foo.a).equal(1)
   expect(out4.bar.b).equal(2)
   expect(out4.rtag$).equal(HIT)
-
 
   foo0.test_no_cache = true
   await foo0.save$() // kicks rtag
@@ -144,16 +148,14 @@ lab.test('resolve', async () => {
   expect(out6.foo.a).equal(1)
   expect(out6.bar.b).equal(2)
   expect(out6.rtag$).equal(MISS)
-
 })
-
 
 lab.test('resolve-entity', async () => {
   var si = seneca_instance()
 
   var HIT = 1
   var MISS = 2
-  
+
   function make_resolver(foo_id) {
     return async function resolver() {
       var foo = await si.entity('foo').load$(foo_id)
@@ -162,9 +164,15 @@ lab.test('resolve-entity', async () => {
     }
   }
 
-  var bar0 = await si.entity('bar').make$({b:1}).save$()
-  var foo0 = await si.entity('foo').make$({handle:'foo0',a:1, bar_id:bar0.id}).save$()
-  
+  var bar0 = await si
+    .entity('bar')
+    .make$({ b: 1 })
+    .save$()
+  var foo0 = await si
+    .entity('foo')
+    .make$({ handle: 'foo0', a: 1, bar_id: bar0.id })
+    .save$()
+
   var out0 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
     rtag: foo0.rtag,
@@ -173,7 +181,7 @@ lab.test('resolve-entity', async () => {
 
   expect(out0.b).equal(1)
   expect(out0.rtag$).equal(MISS)
-  
+
   var out1 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
     rtag: foo0.rtag,
@@ -195,7 +203,6 @@ lab.test('resolve-entity', async () => {
 
   expect(out2.b).equal(2)
   expect(out2.rtag$).equal(MISS)
-  
 
   var out3 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
@@ -206,7 +213,6 @@ lab.test('resolve-entity', async () => {
   expect(out3.b).equal(2)
   expect(out3.rtag$).equal(HIT)
 
-
   var out4 = await si.post('role:cache,resolve:rtag,space:test0', {
     key: foo0.handle,
     rtag: foo0.rtag,
@@ -216,8 +222,6 @@ lab.test('resolve-entity', async () => {
   expect(out4.b).equal(2)
   expect(out4.rtag$).equal(HIT)
 })
-
-
 
 function seneca_instance(config, plugin_options) {
   return Seneca(config, { legacy: { transport: false } })
